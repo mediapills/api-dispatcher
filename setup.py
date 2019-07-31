fa5984a3c7d938ba1e81d9be80319bdd8b733f34
mediapills <andrew.yatskovets@gmail.com>
2019-08-01 01:26:30 +0300

    setup - rename package

--- setup.py
+++ setup.py
@@ -22,11 +22,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='api_dispatcher',
+    name='api-dispatcher',
     version='0.0.1',
     author='Apache Software Foundation',
     author_email='dev@dlab.apache.org',
     url='http://dlab.apache.org/',
-    description='This a provider to DLab that adds AWS support.',
+    description='This a swagger provider.',
     packages=find_packages(),
 )
