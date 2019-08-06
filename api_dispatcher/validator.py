5da021bed8057e385d25f74c23af411a9c2da5d4
ylisovskyi <lisovskyi.yurii@gmail.com>
2019-08-06 15:23:14 +0300

    Schema validator

--- api_dispatcher/validator.py
+++ api_dispatcher/validator.py
@@ -18,7 +18,139 @@
 # under the License.
 #
 # ******************************************************************************
+import json
+import os
+import yaml
+from jsonschema.validators import validator_for
 
 
 class Validator:
-    pass
+    def __init__(self, spec_file):
+        self._spec = Validator.load_file(spec_file)
+        self.schema_path = None
+
+    def validate(self):
+        """Validates given Swagger/OpenAPI specification file """
+        valid_refs = self.validate_refs()
+        valid_spec = self.validate_spec()
+        return -1 if valid_spec < 0 or valid_refs < 0 else 1
+
+    def validate_spec(self):
+        """Validates specification against corresponding schema"""
+        schema = json.load(open(os.path.join(*self.schema_path)))
+        validator = validator_for(schema)
+        errors = validator(schema).iter_errors(self._spec)
+        return -1 if len(list(errors)) > 0 else 1
+
+    def get_all_refs(self, spec, _refs_list=None):
+        """Gets all references from given Swagger/OpenAPI specification file """
+        if isinstance(spec, dict):
+            for k, v in spec.items():
+                if k == "$ref":
+                    _refs_list.append(v)
+                else:
+                    self.get_all_refs(v, _refs_list)
+
+        elif isinstance(spec, list):
+            for item in spec:
+                self.get_all_refs(item, _refs_list)
+
+        return _refs_list
+
+    def validate_refs(self):
+        """Validates all references in specification file
+        """
+        valid = 1
+        refs_list = self.get_all_refs(spec=self._spec, _refs_list=[])
+        for ref in refs_list:
+            valid = self.check_ref(self._spec, ref)
+
+        return valid
+
+    @staticmethod
+    def check_ref(spec, ref):
+        """Validates given reference """
+        path_to_obj = ref.split("/")[1:]
+        item = spec
+        for path in path_to_obj:
+            item = item.get(path)
+            if not item:
+                return -1
+
+        return 1
+
+    @staticmethod
+    def get_spec_validator(file):
+        """Returns validator object for given specification file """
+        spec = Validator.load_file(file)
+        for version_def in VALIDATORS_MAP:
+            if spec.get(version_def):
+                return VALIDATORS_MAP[version_def](spec)
+
+    @staticmethod
+    def load_file(file):
+        """Returns loaded specification file """
+        if isinstance(file, dict):
+            return file
+        if file:
+            if os.path.splitext(file)[1] == '.json':
+                return json.load(open(file))
+            elif os.path.splitext(file)[1] in ('.yml', '.yaml'):
+                return yaml.safe_load(open(file))
+
+
+class Swagger1Validator(Validator):
+
+    def __init__(self, spec_file):
+        super(Swagger1Validator, self).__init__(spec_file)
+        self.schema_path = (
+            os.path.dirname(os.path.realpath(__file__)),
+            "..",
+            "data",
+            "schemas",
+            "v1.2",
+            "apiDeclaration.json"
+        )
+
+
+class Swagger2Validator(Validator):
+
+    def __init__(self, spec_file):
+        super(Swagger2Validator, self).__init__(spec_file)
+        self.schema_path = (
+            os.path.dirname(os.path.realpath(__file__)),
+            "..",
+            "data",
+            "schemas",
+            "v2.0",
+            "schema.json"
+        )
+
+
+class OpenAPIValidator(Validator):
+
+    def __init__(self, spec_file):
+        super(OpenAPIValidator, self).__init__(spec_file)
+        self.schema_path = (
+            os.path.dirname(os.path.realpath(__file__)),
+            "..",
+            "data",
+            "schemas",
+            "v3.0",
+            "schema.json"
+        )
+
+
+def get_spec_validator(file):
+    """Returns validator object for given specification file """
+    spec = Validator.load_file(file)
+    for version_def in VALIDATORS_MAP:
+        if spec.get(version_def):
+            return VALIDATORS_MAP[version_def](spec)
+
+
+VALIDATORS_MAP = {
+    "openapi": OpenAPIValidator,
+    "swagger": Swagger2Validator,
+    "swaggerVersion": Swagger1Validator
+}
