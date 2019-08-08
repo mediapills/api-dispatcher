de4c35004bf4e3505c25fe1422790b022010ea7c
ylisovskyi <lisovskyi.yurii@gmail.com>
2019-08-08 11:45:49 +0300

    Tests refactor

--- tests/unit/test_validator.py
+++ tests/unit/test_validator.py
@@ -27,21 +27,17 @@ from api_dispatcher.validator import get_spec_validator
 def get_test_files(dir_name, include=None, exclude=None):
     all_files = []
     for entry in os.listdir(dir_name):
-        print(entry)
         full_path = os.path.join(dir_name, entry)
-        if os.path.isdir(full_path):
-            if not exclude or (exclude and entry not in exclude):
+        if not exclude or (exclude and entry not in exclude):
+            if os.path.isdir(full_path):
                 _include = None if include and entry in include else include
                 all_files = all_files + get_test_files(
                     full_path,
                     include=_include,
                     exclude=exclude
                 )
-        elif (
-                (not include or (include and entry in include)) and
-                (not exclude or (exclude and entry not in exclude))
-        ):
-            all_files.append(full_path)
+            elif not include or (include and entry in include):
+                all_files.append(full_path)
 
     return all_files
 
@@ -53,30 +49,33 @@ PATH_TO_TEST_FILES = os.path.join(
     "data",
     "examples"
 )
-
-
-@pytest.mark.parametrize("file", get_test_files(
+valid_test_data = get_test_files(
     PATH_TO_TEST_FILES,
     exclude=("petstore-separate", "invalid")
-))
+)
+invalid_test_data = get_test_files(
+    PATH_TO_TEST_FILES,
+    include=("invalid",),
+    exclude=("petstore-without-required-spec-version.yaml",)
+)
+spec_without_version = get_test_files(
+    PATH_TO_TEST_FILES,
+    include=("petstore-without-required-spec-version.yaml",)
+)
+
+
+@pytest.mark.parametrize("file", valid_test_data)
 def test_validator_valid_specs(file):
     validator = get_spec_validator(file)
     assert validator.validate()
 
 
-@pytest.mark.parametrize("file", get_test_files(
-    PATH_TO_TEST_FILES,
-    include=("invalid",),
-    exclude=("petstore-without-required-spec-version.yaml",)
-))
+@pytest.mark.parametrize("file", invalid_test_data)
 def test_validator_invalid_specs(file):
     validator = get_spec_validator(file)
     assert not validator.validate()
 
 
-@pytest.mark.parametrize("file", get_test_files(
-    PATH_TO_TEST_FILES,
-    include=("petstore-without-required-spec-version.yaml",)
-))
+@pytest.mark.parametrize("file", spec_without_version)
 def test_validator_invalid_spec_without_version(file):
     assert not get_spec_validator(file)
